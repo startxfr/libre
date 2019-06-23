2a8efe433b7662de918a24b7a90a58e95fc7e70f
startxfr <clarue@startx.fr>
2019-06-23 13:33:51 +0200

    [libre] Improve log messages

--- ansible/roles/session/files/build-book-html.sh
+++ ansible/roles/session/files/build-book-html.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/bin
-FILE="/usr/share/libre/course/sources/book.asciidoc"
-if [ ! -f "${FILE}" ]
+SOURCE_DIR=/usr/share/libre/course/sources
+SOURCE_FILE=${SOURCE_DIR}/book.asciidoc
+HTML_DIR=/usr/share/libre/course/book
+HTML_FILE=${HTML_DIR}/index.html
+if [ ! -f "${SOURCE_FILE}" ]
 then
-	echo "${FILE} does not exist" >&2
+	echo "${SOURCE_FILE} does not exist" >&2
 	echo "Usage: $0" >&2
 	exit 2
 fi
 
-HTML_FILE=/usr/share/libre/course/book/index.html
-
 rm -f "${HTML_FILE}"
-
 # To HTML
-cd /usr/share/libre/course/sources
-asciidoctor -a lang=fr -a allow-uri-read -b html5 -d book -o "${HTML_FILE}" "${FILE}"
+cd ${SOURCE_DIR}
+asciidoctor -a lang=fr -a allow-uri-read -b html5 -d book -o "${HTML_FILE}" "${SOURCE_FILE}"
+rm -rf ${HTML_DIR}/images
+cp -r ${SOURCE_DIR}/images ${HTML_DIR}/images
 cd -
 echo "HTML: ${HTML_FILE}"
